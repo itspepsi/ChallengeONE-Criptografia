ae536b5d9fc2581bac79be87aa2c4a988d57d8c8
itspepsi <leonardosouzax@live.com>
2022-08-11 18:27:12 -0300

    Decodificador de Texto v1.0

---
+++ index.js
@@ -0,0 +1,45 @@
+let textoEntrada = document.getElementById("entrada");
+let textoSaida = document.getElementById("saida");
+
+let botaoCriptografar = document.getElementById("btn-criptografar");
+let botaoDescriptografar = document.getElementById("btn-descriptografar");
+let botaoCopiar = document.getElementById("btn-copiar");
+
+
+botaoCriptografar.onclick = criptografa;
+botaoDescriptografar.onclick = descriptografa;
+botaoCopiar.onclick = copiar;
+
+function criptografa(){
+    let textoCriptografado = textoEntrada.value;
+
+    let result = textoCriptografado
+    .replaceAll("e", "enter")
+    .replaceAll("i", "imes")
+    .replaceAll("a", "ai")
+    .replaceAll("o", "ober")
+    .replaceAll("u", "ufat");
+
+    textoSaida.value = result;
+}
+
+function descriptografa(){
+    let textoCriptografado = textoEntrada.value;
+
+    let result = textoCriptografado
+    .replaceAll("enter", "e")
+    .replaceAll("imes", "i")
+    .replaceAll("ai", "a")
+    .replaceAll("ober", "o")
+    .replaceAll("ufat", "u");
+
+    textoSaida.value = result;
+}
+
+function copiar() {
+    resultado = document.getElementById("saida");
+
+    resultado.select();
+    document.execCommand("cut")
+}
+
